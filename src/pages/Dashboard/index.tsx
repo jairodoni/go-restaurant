e0c95ef9a24dd8645562c43ef9b9f615442a321a
Jairo Doni <jairo.doni97@gmail.com>
2021-04-07 15:03:45 -0300

    Desafio concluido 🚀

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from "react";
+
+import Header from "../../components/Header";
+import api from "../../services/api";
+import Food from "../../components/Food";
+import ModalAddFood from "../../components/ModalAddFood";
+import ModalEditFood from "../../components/ModalEditFood";
+import { FoodsContainer } from "./styles";
+
+interface IsFood {
+  id: number;
+  name: string;
+  description: string;
+  price: string;
+  available: boolean;
+  image: string;
+}
+
+export default function Dashboard() {
+  const [foods, setFoods] = useState<IsFood[]>([]);
+  const [editingFood, setEditingFood] = useState<IsFood>({} as IsFood);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [editModalOpen, setEditModalOpen] = useState(false);
+
+  useEffect(() => {
+    async function dataFood() {
+      const response = await api.get("/foods");
+      setFoods(response.data);
+    }
+
+    dataFood();
+  }, []);
+
+  async function handleAddFood(food: IsFood): Promise<void> {
+    try {
+      const response = await api.post("/foods", {
+        ...food,
+        available: true,
+      });
+      setFoods([...foods, response.data]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function handleUpdateFood(food: IsFood): Promise<void> {
+    try {
+      if (editingFood) {
+        const foodUpdated = await api.put(`/foods/${editingFood.id}`, {
+          ...editingFood,
+          ...food,
+        });
+
+        const foodsUpdated = foods.map((f) =>
+          f.id !== foodUpdated.data.id ? f : foodUpdated.data
+        );
+
+        setFoods(foodsUpdated);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async function handleDeleteFood(id: number) {
+    await api.delete(`/foods/${id}`);
+
+    const foodsFiltered = foods.filter((food) => food.id !== id);
+
+    setFoods(foodsFiltered);
+  }
+
+  function toggleModal() {
+    setModalOpen(!modalOpen);
+  }
+
+  function toggleEditModal() {
+    setEditModalOpen(!editModalOpen);
+  }
+
+  async function handleEditFood(food: IsFood) {
+    setEditingFood(food);
+    setEditModalOpen(true);
+  }
+
+  return (
+    <>
+      <Header openModal={toggleModal} />
+      <ModalAddFood
+        isOpen={modalOpen}
+        setIsOpen={toggleModal}
+        handleAddFood={handleAddFood}
+      />
+      <ModalEditFood
+        isOpen={editModalOpen}
+        setIsOpen={toggleEditModal}
+        editingFood={editingFood}
+        handleUpdateFood={handleUpdateFood}
+      />
+
+      <FoodsContainer data-testid="foods-list">
+        {foods &&
+          foods.map((food) => (
+            <Food
+              key={food.id}
+              food={food}
+              handleDelete={handleDeleteFood}
+              handleEditFood={handleEditFood}
+            />
+          ))}
+      </FoodsContainer>
+    </>
+  );
+}
